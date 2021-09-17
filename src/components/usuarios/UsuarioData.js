da47edeb76aaeff47a132b3060ff29929a0658af
Juan Manuel Alvarez <juanmanuel@disrupcion.mx>
2021-09-16 23:23:24 -0500

    fixed damage location

--- src/components/usuarios/UsuarioData.js
+++ src/components/usuarios/UsuarioData.js
@@ -2,18 +2,22 @@ import React from "react";
 import moment from "moment";
 
 const UsuarioData = ({ usuario }) => {
+  const renderDamages = () => {
+    if (Array.isArray(usuario.damages)) {
+      return usuario.damages.map((damage) => (
+        <p>{damage.damageLocation.split("_").join(" ")}</p>
+      ));
+    }
+  };
+
   const {
     name,
     email,
     birthDate,
     gender,
     dominantHand,
-    damageLocation,
     antecedent,
-    drugsConsumption,
-    drugsTreatment,
     whichDrugs,
-    dose,
   } = usuario;
   return (
     <div className="row">
@@ -22,12 +26,15 @@ const UsuarioData = ({ usuario }) => {
         <p>Correo: {email}</p>
         <p>Fecha de Nacimiento: {moment(birthDate).format("DD MMM YYYY")}</p>
         <p>Género: {gender}</p>
-        <p>Mano Dominante: {dominantHand}</p>
       </div>
       <div className="col-12 col-md-6">
-        <p>Ubicación del Daño: {damageLocation}</p>
+        <p>Mano Dominante: {dominantHand}</p>
         <p>Antecedentes: {antecedent}</p>
         <p>Drogas o Medicamentos: {whichDrugs}</p>
+      </div>
+      <div className="col-12">
+        <h4>Daños</h4>
+        {renderDamages()}
       </div>
     </div>
   );
